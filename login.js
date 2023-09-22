3ee50e412a891fa39015ae72c9321cbcba718e5c
premkumar118 <145741603+premkumar118@users.noreply.github.com>
2023-09-22 10:31:24 +0530

    initial

---
+++ login.js
@@ -0,0 +1,12 @@
+const conti = () => {
+    var name= document.getElementById("nameb").value
+    var number = document.getElementById("phb").value
+    var mail = document.getElementById("mailb").value
+    var pass = document.getElementById("passb").value
+
+    console.log("NAME : "+name)
+    console.log("MOBILE NUMBER : "+number)
+    console.log("E-MAIL : "+mail)
+    console.log("PASSWORD : "+pass)
+
+}
